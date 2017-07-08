45a816db1284f6d986180e6034ffbcbbaac297bc
flo <florent.pietot@gmail.com>
2017-07-08 19:41:14 +0200

    Create model file and test file for tree model

---
+++ nlp/tree.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Florent Piétot <florent.pietot@gmail.com>
+
+""" Basic tree model we will use for parsing sentence given a ``grammar``
+    We use a subclass of list
+"""
+
+class Tree(list):
+    pass
---
+++ tests/test_tree.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Florent Piétot <florent.pietot@gmail.com>
+
+""" Tests for tree.py module
+"""
+
+import unittest
+from nlp.tree import Tree
+
+class TestTree(unittest.TestCase):
+    pass
+
