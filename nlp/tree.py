e62ddb47fcdab93dd74c3cf6abcdaa9ee8034f27
flo <florent.pietot@gmail.com>
2017-07-10 18:31:39 +0200

    improve test model

--- nlp/tree.py
+++ nlp/tree.py
@@ -5,25 +5,69 @@
 """ Basic tree model we will use for parsing sentences given a ``grammar``
 """
 
-class Tree(list):
+from nlp.tokenizer import tokenize
+
+class Tree(object):
     """ Tree model for building a tree of productions
         Each node is a production
     """
 
     def __init__(self, node=None, children=None):
         self._node = node
-        self._children = childen
+        self._children = children
 
     def node(self):
+        """ Returns the root node of this ``Tree``
+        """
         return self._node
 
     def children(self):
+        """ Returns the children of this ``Tree``
+        """
         return self._children
+
+    def __str__(self):
+        """ Returns a verbose representation of this ``Tree`` as a string
+        """
+        string = "%s" % self._node
+        if self._children:
+            for child in self._children:
+                string += "\n\t%s" % str(child)
+        return string
+
+    def __repr__(self):
+        """ Returns a concise representation of this ``Tree`` as a string
+        """
+        return "(%r, %r)" % (self._node, self._children)
+
+    def __eq__(self, other):
+        """ Return True if this ``Tree`` is equal to ``other``.
+        """
+        return (type(self) == type(other) and
+               self._child == other._child and
+               self._children == other._children)
+
+    def __ne__(self, other):
+        """ Return True if this ``Tree`` is not equal to ``other``.
+        """
+        return not self == other
+
+    def contains(self, elem):
+        pass
 
     def leaves(self):
         """ Returns the leaves of the tree
         """
-        pass
+        leaves = []
+        if self._children:
+            for child in self._children:
+                if isinstance(child, Tree):
+                    leaves += child.leaves()
+                else:
+                    leaves.append(child)
+        else:
+            leaves.append(self._node)
+        return leaves
 
     def make_node(self, children=None):
         pass
@@ -31,7 +75,12 @@ class Tree(list):
     def add_child(self, node, child):
         pass
 
-    @classmethod
-    def make_tree_from_production(self, production):
+
+def tree_from_string(string, grammar=None):
+    if grammar is None:
+        assert ValueError("Grammar param can't be none.")
+    tokens = tokenize(string)
+    for token in tokens:
         pass
+
 
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -14,9 +14,75 @@ class TestTree(unittest.TestCase):
     """
 
     def setUp(self):
-        self.node = "node"
-        self.children = ["child1", "child2", "child3"]
-        self.tree = Tree(self.node, self.children)
+        self.node = "root_node"
+        self.children = [Tree("child1"), Tree("child2"), Tree("child3")]
+        self.tree = Tree(node=self.node, children=self.children)
 
     def test_node(self):
-        assertIsEqual(
+        res = self.node
+        self.assertEqual(res, self.tree.node(), "Should return the root node \
+                      of the tree")
+
+    def test_children(self):
+        res = self.children
+        self.assertEqual(res, self.tree.children(), "Should return \
+                         the children of the tree")
+
+    def test_str_is_string(self):
+        self.assertIsInstance(str(self.tree), str, msg="String expected")
+
+    def test_str(self):
+        # TODO: change this, this is bad
+        res = "root_node\n\tchild1\n\tchild2\n\tchild3"
+        self.assertEqual(res, str(self.tree))
+
+    def test_repr_is_string(self):
+        self.assertIsInstance(repr(self.tree), str, msg="String expected")
+
+    def test_repr(self):
+        res = "(%r, %r)" % (self.node, self.children)
+        self.assertEqual(res, repr(self.tree))
+
+
+class TestLeaves(unittest.TestCase):
+    """ tests for the leaves() function
+    """
+
+    def setUp(self):
+        self.node = "node"
+        self.child_1 = Tree(node="child_1", children=["grandchild1-1",
+                                                "grandchild1-2",
+                                                "grandchild1-3"])
+        self.child_2 = Tree(node="child_2")
+        self.child_3 = "child_3"
+        self.tree = Tree(node=self.node, children=[self.child_1, self.child_2,
+                                                   self.child_3])
+
+    def test_leaves(self):
+        res = []
+        # TODO: improve this (better flattening of all lists)
+        res += self.child_1.children()
+        res.append(self.child_2.node())
+        res.append(self.child_3)
+        self.assertEqual(res, self.tree.leaves())
+
+# class TestAddChild(unittest.TestCase):
+#     """ Tests for the add_child function
+#     """
+
+#     def setUp(self):
+#         self.node = "root_node"
+#         self.children = ["child1", "child2", "child3"]
+#         self.tree = Tree(self.node)
+
+#     def test_add_child(self):
+#         child = "child"
+#         self.tree.add_child(child)
+
+
+class TestTreeFromString(unittest.TestCase):
+    pass
+
+
+if  __name__ == "__main__":
+    unittest.main()
